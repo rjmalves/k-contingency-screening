cc12bbd83e90901ad685504bb18fef2bc9809338
Rogerio Alves <rogerioalves.ee@gmail.com>
2021-10-03 19:41:33 -0300

    Remove loops

--- exaustivo.jl
+++ exaustivo.jl
@@ -1,41 +1,36 @@
 using LightGraphs, GraphIO, ArgParse, BenchmarkTools
 
 
-# function parse_commandline()
-#     s = ArgParseSettings()
-#     @add_arg_table s begin
-#         "grafo"
-#             help = "Arquivo com o grafo em lista de arestas"
-#             arg_type = String
-#             required = true
-#         "k"
-#             help = "Número de arestas removidas simultâneamente"
-#             arg_type = Int
-#             required = true
-#     end
+function parse_commandline()
+    s = ArgParseSettings()
+    @add_arg_table s begin
+        "grafo"
+            help = "Arquivo com o grafo em lista de arestas"
+            arg_type = String
+            required = true
+        "k"
+            help = "Número de arestas removidas simultâneamente"
+            arg_type = Int
+            required = true
+    end
 
-#     return parse_args(s)
-# end
+    return parse_args(s)
+end
 
-# # Echo das entradas
-# args = parse_commandline()
-# for (arg, val) in args
-#     println("$arg  =>  $val")
-# end
+# Echo das entradas
+args = parse_commandline()
+for (arg, val) in args
+    println("$arg  =>  $val")
+end
 
-# ARQ = args["grafo"]
-# K = args["k"]
+ARQ = args["grafo"]
+K = args["k"]
+NOME = string(split(ARQ, ".")[1])
 
 include("cfb.jl")
 using Main.CFB
 
 using GraphPlot
 
-for ARQ in ["ieee118.txt"]
-    for K in [2, 3, 4]
-        println(ARQ, " ", K)
-        NOME = string(split(ARQ, ".")[1])
-        g = Graph(loadgraph(ARQ, NOME, EdgeListFormat()))
-        @btime cfb_exaustivo($g, $K, $NOME)
-    end
-end
+g = Graph(loadgraph(ARQ, NOME, EdgeListFormat()))
+@btime cfb_exaustivo(g, K, NOME)
--- guloso.jl
+++ guloso.jl
@@ -1,40 +1,34 @@
 using LightGraphs, GraphIO, ArgParse, BenchmarkTools
 
 
-# function parse_commandline()
-#     s = ArgParseSettings()
-#     @add_arg_table s begin
-#         "grafo"
-#             help = "Arquivo com o grafo em lista de arestas"
-#             arg_type = String
-#             required = true
-#         "k"
-#             help = "Número de arestas removidas simultâneamente"
-#             arg_type = Int
-#             required = true
-#     end
+function parse_commandline()
+    s = ArgParseSettings()
+    @add_arg_table s begin
+        "grafo"
+            help = "Arquivo com o grafo em lista de arestas"
+            arg_type = String
+            required = true
+        "k"
+            help = "Número de arestas removidas simultâneamente"
+            arg_type = Int
+            required = true
+    end
 
-#     return parse_args(s)
-# end
+    return parse_args(s)
+end
 
-# # Echo das entradas
-# args = parse_commandline()
-# for (arg, val) in args
-#     println("$arg  =>  $val")
-# end
+# Echo das entradas
+args = parse_commandline()
+for (arg, val) in args
+    println("$arg  =>  $val")
+end
 
-# ARQ = args["grafo"]
-# K = args["k"]
-
+ARQ = args["grafo"]
+K = args["k"]
 
 include("cfb.jl")
 using Main.CFB
 
-for ARQ in ["t11.txt", "t39.txt", "t57.txt", "ieee118.txt"]
-    for K in [1, 2, 3, 4]
-        println(ARQ, " ", K)
-        NOME = string(split(ARQ, ".")[1])
-        g = Graph(loadgraph(ARQ, NOME, EdgeListFormat()))
-        @btime cfb_guloso($g, $K, $NOME)
-    end
-end
+NOME = string(split(ARQ, ".")[1])
+g = Graph(loadgraph(ARQ, NOME, EdgeListFormat()))
+@btime cfb_guloso(g, K, NOME)
