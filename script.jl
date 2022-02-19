a595b1f009128604fd2f8585f12ca691857e611c
Rogerio Alves <rogerioalves.ee@gmail.com>
2022-02-19 10:30:13 -0300

    Fix ieee118

--- script.jl
+++ script.jl
@@ -1,34 +1,12 @@
 include("cfb.jl")
 using Main.CFB, BenchmarkTools
 
-
-println("------ t11 -------")
-g = read_edgelist("t11.txt")
-@time cfb_exaustivo(g, 1, "t11")
-@time cfb_exaustivo(g, 2, "t11")
-@time cfb_exaustivo(g, 3, "t11")
-@time cfb_exaustivo(g, 4, "t11")
-
-println("------ t39 -------")
-g = read_edgelist("t39.txt")
-@time cfb_exaustivo(g, 1, "t39")
-@time cfb_exaustivo(g, 2, "t39")
-@time cfb_exaustivo(g, 3, "t39")
-@time cfb_exaustivo(g, 4, "t39")
-
-println("------- t57 --------")
-g = read_edgelist("t57.txt")
-@time cfb_exaustivo(g, 1, "t57")
-@time cfb_exaustivo(g, 2, "t57")
-@time cfb_exaustivo(g, 3, "t57")
-@time cfb_exaustivo(g, 4, "t57")
-
-println("------ t118 -------")
-g = read_edgelist("t118.txt")
-@time cfb_exaustivo(g, 1, "t118")
-@time cfb_exaustivo(g, 2, "t118")
-@time cfb_exaustivo(g, 3, "t118")
-@time cfb_exaustivo(g, 4, "t118")
+println("------ ieee118 -------")
+g = read_edgelist("ieee118.txt")
+@time cfb_exaustivo(g, 1, "ieee118")
+@time cfb_exaustivo(g, 2, "ieee118")
+@time cfb_exaustivo(g, 3, "ieee118")
+@time cfb_exaustivo(g, 4, "ieee118")
 
 println("------ t300 -------")
 g = read_edgelist("t300.txt")
